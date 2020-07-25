b9038d4cddc05d449332ac2854a6b510432b892c
Elamaran <elamaran2595@gmail.com>
2020-07-25 19:10:26 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const cors = require('cors');
+const knex = require('knex');
+const users = require('./controllers/users');
+const add = require('./controllers/add');
+const remove = require('./controllers/remove');
+const update = require('./controllers/update');
+const msgs = require('./controllers/msgs');
+const sendmsg = require('./controllers/sendmsg');
+
+const db=knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'postgres',
+      password : 'mine',
+      database : 'chat_book'
+    }
+  });
+
+const app =express();
+app.use (express.json());
+app.use(cors());
+
+app.get('/users',(req,res)=>{users.handleUsers(req,res,db)});
+
+app.post('/msgs',(req,res)=>{msgs.handleMsgs(req,res,db)});
+
+app.post('/sendmsg',(req,res)=>{sendmsg.handleSendMsg(req,res,db)});
+
+app.post('/add',(req,res)=>{add.handleAdd(req,res,db)});
+
+app.post('/remove',(req,res)=>{remove.handleDelete(req,res,db)});
+
+app.post('/update',(req,res)=>{update.handleUpdate(req,res,db)});
+
+app.listen(3000,()=>{
+    console.log('app is running');
+})
