7e781ecf309d6d531beaa71a61b4daccccf9e5cd
Elamaran <elamaran2595@gmail.com>
2020-07-25 20:51:48 +0530

    db

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 const db=knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : 'postgresql-rigid-26507',
       user : 'postgres',
       password : 'mine',
       database : 'chat_book'
